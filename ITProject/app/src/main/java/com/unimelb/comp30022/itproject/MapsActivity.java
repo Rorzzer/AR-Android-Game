6844e671b5df04864f4b86b5168abbe3218dad41
Connorm1 <connorm1@student.unimelb.edu.au>
2017-09-19 01:29:42 +1000

    Map now show users location

--- ITProject/app/src/main/java/com/unimelb/comp30022/itproject/MapsActivity.java
+++ ITProject/app/src/main/java/com/unimelb/comp30022/itproject/MapsActivity.java
@@ -148,7 +148,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //move map camera
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
 
         //stop location updates
         if (mGoogleApiClient != null) {
