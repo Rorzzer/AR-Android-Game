a2ff60d6bdb5030f16868bd1449e16591f931174
RoryPowell <rpowell@student.unimelb.edu.au>
2017-10-23 02:36:11 +1100

    Sign in fix

--- ITProject/app/src/main/java/com/unimelb/comp30022/itproject/SignInActivity.java
+++ ITProject/app/src/main/java/com/unimelb/comp30022/itproject/SignInActivity.java
@@ -179,6 +179,7 @@ public class SignInActivity extends AppCompatActivity
             showSignedInInterface();
         }
         else {
+            showSignedOutInterface();
             tvStat.setText(SIGN_OUT);
         }
     }
